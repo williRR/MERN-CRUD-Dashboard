842011f4fcf68373c6c1ef073aa0c91d20b8b01b
Willi Roquel <165703502+williRR@users.noreply.github.com>
2024-07-25 03:02:20 +0000

    creando conexion a mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config(); // para leer las variables de entorno
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Crear una instancia de mongoDbclient     
+let client;
 
-app.listen(port, () => console.log('Server is running on port 3000' , port));
+async function connectToMongoDB() {
+    try {
+        client = new MongoClient(process.env.MONGODB_URI);
+        await client.connect();
+        console.log('Conectado a MongoDB Atlas');
+    } catch (error) {
+        console.error('ERROR AL CONECTAR', error);
+    }
+}
+
+// Conectar a MongoDB
+connectToMongoDB();
+
+// Rutas
+app.get('/', (req, res) => {
+    res.send('Hello World hola ');
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
