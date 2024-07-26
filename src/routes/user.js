475370f6f2c4e1df4cfaddfe7519dac553f48e44
Willi Roquel <165703502+williRR@users.noreply.github.com>
2024-07-26 02:55:52 +0000

    agregando funciones crud

--- src/routes/user.js
+++ src/routes/user.js
@@ -12,5 +12,52 @@ router.post("/user", (req, res) => {
         .catch((error) => res.json({message: error}));
 });
 
-module.exports = router;
 
+
+
+// mostar todos los usuarios
+
+router.get("/user", (req, res) => {
+    userSchema
+        .find()
+        .then((data)=> res.json(data))
+        .catch((error) => res.json({message: error}));
+});
+
+// mostrar un usuario en especifico
+
+
+router.get("/user/:id", (req, res) => {
+    const{id}= req.params
+    userSchema
+        .findById(id)
+        .then((data)=> res.json(data))
+        .catch((error) => res.json({message: error}));
+});
+
+
+// actualizar un usuario en especifico
+router.put("/user/:id", (req, res) => {
+    const{id}= req.params;
+    const{name,age,email}= req.body;
+    userSchema
+        .updateOne({_id:id},{  $set: {name,age,email}})
+        .then((data)=> res.json(data))
+        .catch((error) => res.json({message: error}));
+});
+
+
+
+// eliminar un usuario en especifico
+
+router.delete("/user/:id", (req, res) => {
+    const{id}= req.params;
+    userSchema
+        .deleteOne({_id:id})        
+        .then((data)=> res.json(data))
+        .catch((error) => res.json({message: error}));
+});
+
+
+
+module.exports = router;
